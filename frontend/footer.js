41fe906aa8901ac6ebd0677e3c3cb84f07e4d131
SriHarsha379 <harshas379@gmail.com>
2026-01-09 18:15:59 +0530

    Added common header and footer to all pages

---
+++ frontend/footer.js
@@ -0,0 +1,34 @@
+document.querySelector("footer").innerHTML = `
+<div class="footer-inner">
+
+  <div>
+    <div class="logo">
+      <a href="index.html">
+        <img src="images/IMG_2280.JPG">
+      </a>
+    </div>
+    <p style="margin-top:10px">
+      Media Buzz 365 News – Latest Telugu Breaking News, Live TV & Special Stories.
+    </p>
+  </div>
+
+  <div>
+    <a href="about.html">About</a>
+    <a href="contact.html">Contact</a>
+    <a href="advertise.html">Advertise</a>
+    <a href="privacy.html">Privacy Policy</a>
+  </div>
+
+  <div>
+    <a href="https://www.facebook.com/RTVTeluguDigital/" target="_blank">Facebook</a>
+    <a href="https://www.instagram.com/rtvnewsnetwork/" target="_blank">Instagram</a>
+    <a href="https://www.youtube.com/channel/UCE5RIa7b_JIK0a31Uaq4xRw" target="_blank">YouTube</a>
+    <a href="https://www.whatsapp.com/channel/0029Va9lQhBGk1Fr2DHRUO1U" target="_blank">WhatsApp</a>
+  </div>
+
+</div>
+
+<div class="copy">
+© 2025 DHAATRI MEDIA BROADCAST PRIVATE LIMITED
+</div>
+`;
